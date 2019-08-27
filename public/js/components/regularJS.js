54dbb533804b48f3cd672713f9cce80aa3063080
Artem Todorov <artemtodorov@Maxs-MacBook-Air.local>
2019-08-27 13:08:35 -0700

    adding sideMenu

--- public/js/components/regularJS.js
+++ public/js/components/regularJS.js
@@ -1,13 +1,10 @@
 webpackJsonp([0],{
 
-/***/ 99:
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
+/***/ 234:
+/***/ (function(module, exports) {
 
 console.log('Welcome To The Rocky Stack');
 
 /***/ })
 
-},[99]);
+},[234]);
